08863a113afb680a553c8000a555110ed80750b2
0penth3wind0w <fc0029@yahoo.com.tw>
2018-04-20 02:19:13 +0800

    minor modification

--- app.py
+++ app.py
@@ -57,6 +57,5 @@ def handle_text_message(event):                  # default
 
 # ================= BOT End =================
 
-
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0',port=os.environ['PORT'])
