c1e9eb3d32b0e35df2dc60007104c7dda2aa1b23
0penth3wind0w <fc0029@yahoo.com.tw>
2018-04-20 23:57:21 +0800

    add some reply message

--- app.py
+++ app.py
@@ -2,8 +2,11 @@
 import os
 import requests
 from flask import Flask, request, abort
+
+# functions
 from response import response
 
+# LINE
 from linebot import (
 	LineBotApi, WebhookHandler
 )
@@ -11,7 +14,10 @@ from linebot.exceptions import (
 	InvalidSignatureError
 )
 from linebot.models import (
-	MessageEvent, TextMessage, TextSendMessage, FollowEvent,
+	MessageEvent,
+	TextMessage, TextSendMessage,
+	StickerMessage, StickerSendMessage,
+	FileMessage, FollowEvent,
 )
 
 ACCESS_TOKEN = os.environ.get('CHANNEL_ACCESS_TOKEN')
@@ -44,23 +50,21 @@ def callback():
 	return 'OK'
 
 # ================= Bot Start =================
+# Reply to text message
 @handler.add(MessageEvent, message=TextMessage)  # default
 def handle_text_message(event):                  # default
-	msg = event.message.text #message from user
-	reply_msg = response(msg)
-	# 針對使用者各種訊息的回覆 Start =========
-	line_bot_api.reply_message(
-		event.reply_token,
-		TextSendMessage(text=reply_msg))
-	# 針對使用者各種訊息的回覆 End =========
+	replyText(event)
+	
 
+# Greeting messages when user add this bot
 @handler.add(FollowEvent)
 def handle_follow(event):
 	profile = line_bot_api.get_profile(event.source.user_id)
-	greeting_msg = profile.display_name+"你好～我是彥霖的自我介紹機器人，很高興認識你(blush)\n請問你想之知道什麼呢？"
+	greeting_msg = profile.display_name+"你好～我是彥霖的自我介紹機器人，很高興認識你\n請問你想之知道什麼呢？"
 	line_bot_api.reply_message(
 		event.reply_token,
 		TextSendMessage(text=greeting_msg))
+	#line_bot_api.push_message(profile.user_id,)
 # ================= BOT End =================
 
 if __name__ == "__main__":
--- response.py
+++ response.py
@@ -1,5 +1,45 @@
-def response(input_msg):
-    answer = ''
-    if ("學歷" in input_msg):
-        answer += "我目前就讀於北科大的資訊工程系研究所\n大學則是就讀國立臺北大學，主修資訊工程，並雙主修金融與合作經營。"
-    return answer
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent,
+    TextMessage, TextSendMessage,
+    StickerMessage, StickerSendMessage,
+    FileMessage, FollowEvent,
+)
+
+def replyText(event):
+    profile = line_bot_api.get_profile(event.source.user_id)
+    msg = event.message.text #message from user
+    
+    if ("學歷" or "學校" or "就讀") in msg:
+        reply_msg = "我目前就讀於北科大的資訊工程系研究所\n大學則是就讀國立臺北大學，主修資訊工程，並雙主修金融與合作經營。"
+        line_bot_api.push_message(
+            profile.user_id,
+            TextSendMessage(text=reply_msg))
+    if ("工作" or "實習") in msg:
+        reply_msg = "大學的寒暑假時，我曾經去巨司文化（數位時代、經理人）實習。實習的時候主要負責網站的維護"()
+        line_bot_api.push_message(
+            profile.user_id,
+            TextSendMessage(text=reply_msg)
+        )
+    if (("程式" and "語言") or ("用" and "語言")) in msg:
+        reply_msg = "我會的程式語言有C/C++,Python\n也曾經接觸過一點點的Ruby on Rails和JavaScript喔"
+        line_bot_api.push_message(
+            profile.user_id,
+            TextSendMessage(text=reply_msg)
+        )
+    if ("履歷" or "簡歷" or "自傳") in msg:
+        reply_msg = "等我一下喔～我把我的自傳傳給你，裡面有更多詳細的資料唷"
+        line_bot_api.push_message(
+            profile.user_id,
+            TextSendMessage(text=reply_msg)
+        )
+    else:
+        reply_msg = "Sorry"
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_msg))
