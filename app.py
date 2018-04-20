3004d0a276b71035b86352dfe9718121d3d1b320
0penth3wind0w <fc0029@yahoo.com.tw>
2018-04-21 00:08:54 +0800

    minor bug fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import requests
 from flask import Flask, request, abort
 
 # functions
-from response import replyText
+#from response import replyText
 
 # LINE
 from linebot import (
@@ -69,3 +69,36 @@ def handle_follow(event):
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',port=os.environ['PORT'])
+
+def replyText(event):
+	profile = line_bot_api.get_profile(event.source.user_id)
+	msg = event.message.text #message from user
+	
+	if ("學歷" or "學校" or "就讀") in msg:
+		reply_msg = "我目前就讀於北科大的資訊工程系研究所\n大學則是就讀國立臺北大學，主修資訊工程，並雙主修金融與合作經營。"
+		line_bot_api.push_message(
+			profile.user_id,
+			TextSendMessage(text=reply_msg))
+	if ("工作" or "實習") in msg:
+		reply_msg = "大學的寒暑假時，我曾經去巨司文化（數位時代、經理人）實習。實習的時候主要負責網站的維護"()
+		line_bot_api.push_message(
+			profile.user_id,
+			TextSendMessage(text=reply_msg)
+		)
+	if (("程式" and "語言") or ("用" and "語言")) in msg:
+		reply_msg = "我會的程式語言有C/C++,Python\n也曾經接觸過一點點的Ruby on Rails和JavaScript喔"
+		line_bot_api.push_message(
+			profile.user_id,
+			TextSendMessage(text=reply_msg)
+		)
+	if ("履歷" or "簡歷" or "自傳") in msg:
+		reply_msg = "等我一下喔～我把我的自傳傳給你，裡面有更多詳細的資料唷"
+		line_bot_api.push_message(
+			profile.user_id,
+			TextSendMessage(text=reply_msg)
+		)
+	else:
+		reply_msg = "Sorry"
+		line_bot_api.reply_message(
+			event.reply_token,
+			TextSendMessage(text=reply_msg))
