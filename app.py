efa6980f81f7b2431af3f7b9996262c18dfa481c
0penth3wind0w <fc0029@yahoo.com.tw>
2018-04-20 11:53:53 +0800

    use tab

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import requests
 from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi, WebhookHandler
+	LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+	InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, FollowEvent,
+	MessageEvent, TextMessage, TextSendMessage, FollowEvent,
 )
 
 ACCESS_TOKEN = os.environ.get('CHANNEL_ACCESS_TOKEN')
@@ -24,40 +24,41 @@ line_bot_api = LineBotApi(ACCESS_TOKEN)
 
 @app.route('/')
 def index():
-    return "<p>Hello World!</p>"
+	return "<p>Hello World!</p>"
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
+	# get X-Line-Signature header value
+	signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+	# get request body as text
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+	# handle webhook body
+	try:
+		handler.handle(body, signature)
+	except InvalidSignatureError:
+		abort(400)
 
-    return 'OK'
+	return 'OK'
 
 # ================= Bot Start =================
 @handler.add(MessageEvent, message=TextMessage)  # default
 def handle_text_message(event):                  # default
-    msg = event.message.text #message from user
+	msg = event.message.text #message from user
 	profile = line_bot_api.get_profile(event.source.userId)
-    # 針對使用者各種訊息的回覆 Start =========
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=msg))
+	# 針對使用者各種訊息的回覆 Start =========
+	line_bot_api.reply_message(
+		event.reply_token,
+		TextSendMessage(text=msg))
     
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=profile.displat_name))
+	line_bot_api.reply_message(
+		event.reply_token,
+		TextSendMessage(text=profile.displat_name))
 
-    # 針對使用者各種訊息的回覆 End =========
+	# 針對使用者各種訊息的回覆 End =========
+
 
 '''@handler.add(FollowEvent)
 def handle_follow_event(event):
@@ -71,4 +72,4 @@ def handle_follow_event(event):
 # ================= BOT End =================
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=os.environ['PORT'])
+	app.run(host='0.0.0.0',port=os.environ['PORT'])
