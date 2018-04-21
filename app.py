697db54c3c8d244e90db9e2a08df95ca4122c8ab
0penth3wind0w <fc0029@yahoo.com.tw>
2018-04-21 22:50:08 +0800

    test regex

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ class Reply(Event):
 	def reply_to_usr(self):
 		replied = False
 		msg = self.event.message.text #message from user
-		if bool(re.search("[hi|Hello|你好|嗨|哈囉]", msg)):
+		if bool(re.search("[(hi)|(Hello)|(你好)|(嗨)|(哈囉)]", msg)):
 			msgs = ['hi', 'Hello', "你好", "嗨", "哈囉"]
 			reply_msg = random.choice(msgs) + "～"
 			msgObj = TextSendMessage(text=reply_msg)
@@ -126,48 +126,3 @@ class Reply(Event):
 		line_bot_api.reply_message(
 				self.event.reply_token,
 				msg)
-
-
-def replyText(event):
-	replied = False
-	profile = line_bot_api.get_profile(event.source.user_id)
-	msg = event.message.text #message from user
-	if bool(re.search("[hi|Hello|你好|嗨|哈囉]", msg)):
-		msgs = ['hi', 'Hello', "你好", "嗨", "哈囉"]
-		reply_msg = random.choice(msgs) + "～"
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(text=reply_msg))
-		replied = True
-	if bool(re.search("[學歷|學校|就讀|大學|研究所]", msg)):
-		reply_msg = "我目前就讀於北科大的資訊工程系研究所\n大學則是就讀國立臺北大學，主修資訊工程，並雙主修金融與合作經營。"
-		line_bot_api.push_message(
-			profile.user_id,
-			TextSendMessage(text=reply_msg))
-		replied = True
-	if bool(re.search("[工作|實習]", msg)):
-		reply_msg = "大學的寒暑假時，我曾經去巨司文化（數位時代、經理人）實習。實習的時候主要負責網站的維護"
-		line_bot_api.push_message(
-			profile.user_id,
-			TextSendMessage(text=reply_msg))
-		replied = True
-	if bool(re.search("[程式.語言|語言.程式|用.語言]", msg)):
-		reply_msg = "我會的程式語言有C/C++,Python\n也曾經接觸過一點點的Ruby on Rails和JavaScript喔"
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(text=reply_msg))
-		replied = True
-	if bool(re.search("[履歷|簡歷|自傳]", msg)):
-		reply_msg = "等我一下喔～我把我的自傳傳給你，裡面有更多詳細的資料唷"
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(text=reply_msg))
-		replied = True
-	if not replied:
-		reply_msg = "對不起，我現在還不會回答這個問題\nQ_Q"
-		line_bot_api.reply_message(
-			event.reply_token,
-			TextSendMessage(text=reply_msg))
-		line_bot_api.push_message(
-			profile.user_id,
-			StickerSendMessage(package_id=2,sticker_id=153))
