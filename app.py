03d4612b3eab8dc040ae83ae2156bb16c345721f
0penth3wind0w <fc0029@yahoo.com.tw>
2018-04-22 10:56:32 +0800

    add carousel image

--- app.py
+++ app.py
@@ -56,11 +56,11 @@ def handle_text_message(event):
 @handler.add(FollowEvent)
 def handle_follow(event):
 	profile = line_bot_api.get_profile(event.source.user_id)
+	line_bot_apilink_rich_menu_to_user( profile.user_id, rich_menu_id, timeout=None)
 	greeting_msg = profile.display_name+"你好～我是自我介紹機器人Freddy，很高興認識你\n請先試試看各項功能吧"
 	line_bot_api.reply_message(
 		event.reply_token,
 		TextSendMessage(text=greeting_msg))
-	
 	#line_bot_api.push_message(profile.user_id,)
 
 @handler.add(JoinEvent)
@@ -155,7 +155,7 @@ example = TemplateSendMessage(
 						text="有沒有提供簡歷呢？"),])])
 )
 # Rich Menu
-'''rich_menu_create = RichMenu(
+rich_menu = RichMenu(
 	size=RichMenuBound(
 		width=2500,
 		height=1686),
@@ -171,8 +171,8 @@ example = TemplateSendMessage(
 			URITemplateAction(
 				uri='line://nv/location'))]
 )
-rich_menu_id = line_bot_api.create_rich_menu(data=rich_menu_to_create)
-'''
+rich_menu_id = line_bot_api.create_rich_menu(data=rich_menu)
+
 # Use to reply users
 class Reply(Event):
 	def __init__(self, event=None):
