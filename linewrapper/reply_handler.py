d7a743ba58915ffa0e62a10b0d8bbdfa72935fe8
0penth3wind0w <fc0029@yahoo.com.tw>
2021-09-23 23:55:46 +0800

    remove log

--- linewrapper/reply_handler.py
+++ linewrapper/reply_handler.py
@@ -19,13 +19,9 @@ class Reply(Event):
         if any(keyword in message for keyword in ['wallpaper']):
             wallpaper_url = get_wp_url()
             profile = line_bot_api.get_profile(self.event.source.user_id)
-            print('log profile')
-            print(profile)
             imageMsg = ImageSendMessage(wallpaper_url, wallpaper_url)
             line_bot_api.reply_message(self.event.reply_token, imageMsg)
             textMsg = TextSendMessage(text=wallpaper_url)
-            print('log user')
-            print(profile.user_id)
             line_bot_api.push_message(profile.user_id, textMsg)
             replied = True
         if not replied:
